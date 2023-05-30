79626c9566c01c1a45cf86bec1a4c2b7815fbdde
Mikhail Bolotov <mikhail.bolotov@gmail.com>
2023-05-30 21:50:26 +0300

    We need to support refactoring fetures of IDE (fixes #24)

--- src/main/kotlin/com/broxus/solidity/ide/refactoring/SolMoveHandler.kt
+++ src/main/kotlin/com/broxus/solidity/ide/refactoring/SolMoveHandler.kt
@@ -11,7 +11,7 @@ class SolMoveHandler : MoveFilesOrDirectoriesHandler() {
   override fun doMove(project: Project?, elements: Array<out PsiElement>, targetContainer: PsiElement?, callback: MoveCallback?) {
     val oldRefs = mutableMapOf<String, MutableList<PsiReference>>()
     elements.forEach { oldFile ->
-      ReferencesSearch.search(oldFile).forEach { oldRefs.getOrPut(oldFile.containingFile.name) { mutableListOf() }.add(it) }
+      ReferencesSearch.search(oldFile).forEach {ref ->  oldFile.containingFile?.let { oldRefs.getOrPut(it.name) { mutableListOf() }.add(ref) } }
     }
 
     super.doMove(project, elements, targetContainer) {
